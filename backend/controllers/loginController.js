4f27304fc71e310c6ef6c8a6c4b30ecf30db5049
linusmtz <linusmartinez00@gmail.com>
2023-08-12 00:05:39 -0400

    login controller updated

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -5,24 +5,27 @@ var bcryptjs = require('bcryptjs');
 
 async function login(req,res){
     try{
-        var {email,password} = req.body;
-        let user = UserModel.findOne({email:email});
-
+        var {username,password} = req.body;
+        let user = await UserModel.findOne({username:username});
 
         if(!user){
-            res.status(400).send({msg:"user not found",status:400});
+            res.status(400).send({msg:"user not found",estatus:0});
             throw new Eror('User not registered')
         }else{
+            const result = await bcryptjs.compare(password,user.password);
             
+            if(!result){
+                res.status(400).send({msg:"Wrong password",estatus:0});
+                throw new Error('Wrong password')
+            }else{
+                res.status(200).send({msg:"Logged in",estatus:1});
+            }
         }
-
-
     }catch(error){
         console.log(error);
-
     }
+}
 
-
-
-
+module.exports= {
+    login
 }
